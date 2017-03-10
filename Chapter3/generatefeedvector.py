ca114e5adb27837e51fa68e4945c0a02de43e15b
macoskey <macoskey@umich.edu>
2017-03-09 22:58:19 -0500

    data generated from blogs

--- Chapter3/generatefeedvector.py
+++ Chapter3/generatefeedvector.py
@@ -2,7 +2,7 @@ import feedparser
 import re
 
 # returns title and dictionary of word counts for an RSS feed
-def getworkdcounts(url):
+def getwordcounts(url):
 	# parse the feed
 	d = feedparser.parse(url)
 	wc = {}
@@ -10,16 +10,16 @@ def getworkdcounts(url):
 	# loop over all the entries
 	for e in d.entries:
 		if 'summary' in e:
-			ummary=e.summary
+			summary = e.summary
 		else:
 			summary = e.description
 
 		# extract a list of words
 		words = getwords(e.title+' '+summary)
-		for words in words:
+		for word in words:
 			wc.setdefault(word,0)
-			wc[word]+=1
-	return d.feed.title,wc
+			wc[word] += 1
+	return (d.feed.title,wc)
 
 def getwords(html):
 	# remove the HTML tags
@@ -29,25 +29,43 @@ def getwords(html):
 	words = re.compile(r'[^A-Z^a-z]+').split(txt)
 
 	# convert to lowercase
-	return [word.lower() for word in words if word!='']
+	return [word.lower() for word in words if word != '']
 
 
 
 apcount = {}
 wordcounts = {}
-feedlist = []
-for feedurl in file('feedlist.txt'):
-	feedlist.add(feedurl)
-	title,wc = getwordcounts(feedurl)
-	wordcounts[title] = wc
-	for word, count in wc.items():
-		apcount.setdefault(word,0)
-		if count > 1:
-			apcount[word]+=1
+si = 1
+feedlist = [line for line in file('feedlist.txt')]
+for feedurl in feedlist:
+    try:
+        (title,wc) = getwordcounts(feedurl)
+        wordcounts[title] = wc
+        for word, count in wc.items():
+	    apcount.setdefault(word,0)
+	    if count > 1:
+                apcount[word]+=1
+        print '%s successful' % si
+        si += 1
+    except:
+        print 'Failed to parse feed %s' % feedurl
 
+        
 wordlist = []
 for w, bc in apcount.items():
-	frac = fload(bc)/len(feedlist)
+	frac = float(bc)/len(feedlist)
 	if frac > 0.1 and frac < 0.5:
 		wordlist.append(w)
-		
+	
+out = file('blogdata.txt','w')
+out.write('Blog')
+for word in wordlist: out.write('\t%s' % word)
+out.write('\n')
+for blog,wc in wordcounts.items():
+    # ideal with univode outside the ASCII range
+    blog = blog.encode('ascii','ignore')
+    out.write(blog)
+    for word in wordlist:
+        if word in wc: out.write('\t%d' % wc[word])
+        else: out.write('\t0')
+    out.write('\n')
