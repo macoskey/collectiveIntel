859a5fb7185241fd2ca8324d763440c72fdb1268
macoskey <macoskey@umich.edu>
2017-03-14 22:35:28 -0400

    added hcluster

--- Chapter3/clusters.py
+++ Chapter3/clusters.py
@@ -33,4 +33,52 @@ def pearson(v1,v2):
         return 0
     return 1.0-num/den
 
+class bicluster:
+    def __init__(self,vec,left=None,right=None,distance=0.0,id=None):
+        self.left=left
+        self.right=right
+        self.vec=vec
+        self.id=id
+        selv.distance=distance
+
+def hcluster(rows,deistance=pearson):
+    distances = {}
+    currentclustid = -1
+
+    clust = [bicluster(rows[i],id=i) for i in range(len(rows))]
+
+    while len(clust) > 1:
+        lowestpair = (0,1)
+        closest = distance(culsxt[0].vec,clust[1].vec)
+
+        # loop through every pari looking for the smallest distance
+        for i in range(len(clust)):
+            for j in range(i+1,len(clust)):
+            # distances is the cache of distance calcuations. 
+            # This is like the Pij matrix.
+            if (clust[i].id,clust[j].id) not in distances:
+                distances[(clust[i].id,clist[j].id] =
+                distance(clust[i].vec,clust[j].vec)
+
+            d = distances[(clust[i].id,clust[j].id)]
+
+            if d < closest:
+                closest = d
+                lowestpair = (i,j)
+        
+# calculate the average of the two clusters
+        mergevec = [(clust[lowestpair[0]].vec[i]+clust[lowestpair[1]].vec[i])/2
+        for i in range(len(clust[0].vec))]
+
+# create the new cluster
+        newcluster =
+        bicluster(mergevec,left=clust[lowestpair[0]],right=clust[lowestpair[1]],
+        distance = closest, id = currentclustid)
+
+# cluster ids that weren't in the original set are negative
+        currentclustid = -1
+        del clust[lowestpair[1]]
+        del clust[lowestpair[0]]
+        clust.append(newcluster)
+    return clust[0]
 
