887481bd83bdd5bd922bec1624163d095c76ceb4
macoskey <macoskey@umich.edu>
2017-04-05 23:38:12 -0400

    sleep

--- Chapter4/searchengine.py
+++ Chapter4/searchengine.py
@@ -1,6 +1,13 @@
 # search engine script inspired by Collective Intelligence
 # J. Macoskey
 
+import urllib2
+import urljoin
+import sqlite3
+from bs4 import *
+
+ignorewords = set(['the','of','to','and','a','in','is','it'])
+
 class crawler:
      # initialize the crawler with the name of the database
      def __init__(self,dbname): # class initialization
@@ -32,7 +39,7 @@ class crawler:
     # return true if this url is already indexed
     def isindexed(self,url):
         return False
-    
+    	
     # add a link between two pages
     def addlinkref(self,urlFrom,urlTo,linkText):
         pass
@@ -40,7 +47,22 @@ class crawler:
     # starting with a list of pages, do a breadth first search to the given
     # depth, indexing pages as we go
     def crawl(self,pages,depth = 2):
-        pass
+        for i in range(depth):
+            newpages = set();
+            for page in pages:
+                try:
+                    c = urllib2.urlopen(page)
+                except:
+                    print "Could not open %s" % page
+                    continue
+                soup = BeautifulSoup(c.read())
+                self.addtoindex(page,soup)
+
+                links = soup('a')
+                for link in links:
+                    if ('href' in dict(link.attrs)):
+                        url = urljoin(page,link['href'])
+
 
     # create the database tables
     def createindextables(self):
