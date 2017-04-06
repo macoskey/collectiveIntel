693a071c9784c98b640b262961e919203b83d601
macoskey <macoskey@umich.edu>
2017-04-05 23:17:07 -0400

    crawler skeleton

--- Chapter4/searchengine.py
+++ Chapter4/searchengine.py
@@ -18,5 +18,32 @@ class crawler:
         return None
 
     # index and individual page
-    def 
-         
+    def addtoindex(self,url,soup):
+        print 'Indexing %s' % url
+
+    # extract the text from an HTML page (no tags)
+    def gettextonly(self,soup):
+        return None
+
+    # separate the words by any non-whitespace character
+    def separatewords(self,text):
+        return None
+
+    # return true if this url is already indexed
+    def isindexed(self,url):
+        return False
+    
+    # add a link between two pages
+    def addlinkref(self,urlFrom,urlTo,linkText):
+        pass
+
+    # starting with a list of pages, do a breadth first search to the given
+    # depth, indexing pages as we go
+    def crawl(self,pages,depth = 2):
+        pass
+
+    # create the database tables
+    def createindextables(self):
+        pass
+
+
