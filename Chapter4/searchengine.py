a34b60b9142e079fa79dc768cc6117292b50b3b8
macoskey <macoskey@umich.edu>
2017-04-05 23:12:58 -0400

    begin chapter 4

---
+++ Chapter4/searchengine.py
@@ -0,0 +1,22 @@
+# search engine script inspired by Collective Intelligence
+# J. Macoskey
+
+class crawler:
+     # initialize the crawler with the name of the database
+     def __init__(self,dbname): # class initialization
+        pass
+
+    def __del__(self): # class deletion parameters
+        pass
+
+    def dbcommit(self):
+        pass
+
+    # auxilliary function for getting an entry id and adding it if it's not
+    # present
+    def getentryid(self,table,field,value,createnew=True):
+        return None
+
+    # index and individual page
+    def 
+         
